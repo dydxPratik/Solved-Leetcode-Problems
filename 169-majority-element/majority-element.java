72060ced9b6da67afd8226224802d65351c14aeb
dydxPratik <121711799+dydxPratik@users.noreply.github.com>
2023-08-04 00:09:21 +0530

    Time: 1 ms (99.84%) | Memory: 48.6 MB (51.08%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,48 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        if(nums.length == 0) return -1;;
-        HashMap<Integer , Integer> hmap = new HashMap<>();
-        for(int i = 0; i < nums.length ; i++)
+        int ele = 0;
+        int count = 0;
+
+        for(int i  = 0 ; i < nums.length; i++)
         {
-            if(hmap.containsKey(nums[i])) 
+            if(count == 0)
             {
-                hmap.put(nums[i] , hmap.get(nums[i]) + 1);
+                ele = nums[i];
+                count = 1;
+            }
+
+            else if(nums[i] != ele)
+            {
+                count--;
             }
             else
             {
-                hmap.put(nums[i] , 1); 
+                count++;
             }
         }
-        
-
-        for(Map.Entry<Integer , Integer> entry : hmap.entrySet())
-        {
-            if(entry.getValue() > nums.length / 2 ) return entry.getKey();
-        }
-
-        return -1;
+        return ele;
     }
 }
-
-
-
-// class Solution {
-//     public int majorityElement(int[] nums) {
-//         if (nums.length == 0) return -1;
-//         HashMap<Integer, Integer> hmap = new HashMap<>();
-
-//         for (int i = 0; i < nums.length; i++) {
-//             if (hmap.containsKey(nums[i])) {
-//                 hmap.put(nums[i], hmap.get(nums[i]) + 1); // Increment the count for existing key
-//             } else {
-//                 hmap.put(nums[i], 1); // Add a new key with count 1
-//             }
-//         }
-
-//         for (Map.Entry<Integer, Integer> entry : hmap.entrySet()) {
-//             if (entry.getValue() > nums.length / 2) return entry.getKey();
-//         }
-
-//         return -1;
-//     }
-// }
